76272b9d4be5bf2085cf8ba794b32f10f17e0d7b
Hunter <hgouldthorpe95@gmail.com>
2023-12-29 20:26:00 -0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+let currentWeapon = 0;
+let fighting;
+let gold = 50; 
+const goldText = document.querySelector("#goldText");
+let health = 100;
+const healthText = document.querySelector("#healthText");
+let inventory = ["stick"];
+let monsterHealth;
+const monsterHealthText = document.querySelector("#monsterHealth");
+const monsterName = document.querySelector("#monsterName");
+const monsterStats = document.querySelector("#monsterStats");
+const text = document.querySelector("#text");
+let xp = 0;
+const xpText = document.querySelector("#xpText");
