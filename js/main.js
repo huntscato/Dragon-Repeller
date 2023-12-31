20b36247611cca7785e63e719d6399be4385a497
Hunter <hgouldthorpe95@gmail.com>
2023-12-31 11:54:11 -0500

    Attempting to build responsive buttons

--- js/main.js
+++ js/main.js
@@ -15,3 +15,31 @@ const monsterStats = document.querySelector("#monsterStats");
 const text = document.querySelector("#text");
 let xp = 0;
 const xpText = document.querySelector("#xpText");
+
+
+// Initialize Buttons 
+
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+
+
+function goStore() {
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+}
+
+
+function goCave() {
+    console.log("Going to cave.");
+}
+
+
+function fightDragon() {
+    console.log("Fighting dragon.");
+}
